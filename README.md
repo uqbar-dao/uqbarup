685b921a15e814b36549016679cf827b51d4ae23
dachus <dachus@dachus>
2023-10-03 19:42:29 +0200

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Uqbarup : Uqbar Build and Install tools
+inspiration from [foundry](https://github.com/foundry-rs/foundry)
+
+# TODO
+- `wasi_snapshot_preview1.wasm` and wit files need to be grabbed from some canonical location, cannot live here independently
+    - possibly version them see [this](https://component-model.bytecodealliance.org/creating-and-consuming/composing.html)
+- prompt users to install the following
+    - wasm-tools
+    - cargo
+- get init/install/uqbar working
---
+++ uqbar/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+BASE_DIR=${XDG_CONFIG_HOME:-$HOME}
+UQBAR_DIR=${UQBAR_DIR-"$BASE_DIR/.uqbar"}
+
+pwd=$(pwd)
+name=$(grep -o 'name = "[^"]*' ./Cargo.toml | sed 's/name = "//')
+
+rm -rf "$pwd/wit"
+cp -r "$UQBAR_DIR/wit" "$pwd"
+mkdir -p "$pwd/target/bindings/$name"
+
+cp "$UQBAR_DIR/target.wasm" "$pwd/target/bindings/$name/"
+cp "$UQBAR_DIR/world" "$pwd/target/bindings/$name/"
+cp "$UQBAR_DIR/wasi_snapshot_preview1.wasm" $pwd
+
+mkdir -p "$pwd/target/wasm32-unknown-unknown/release"
+
+# Build the module using Cargo
+cargo build \
+    --release \
+    --no-default-features \
+    --manifest-path="$pwd/Cargo.toml"\
+    --target "wasm32-wasi"
+
+# Adapt the module using wasm-tools
+wasm-tools component new "$pwd/target/wasm32-wasi/release/$name.wasm" -o "$pwd/target/wasm32-wasi/release/${name}_adapted.wasm" --adapt "$pwd/wasi_snapshot_preview1.wasm"
+
+# Embed "wit" into the component and place it in the expected location
+wasm-tools component embed wit --world uq-process "$pwd/target/wasm32-wasi/release/${name}_adapted.wasm" -o "$pwd/target/wasm32-unknown-unknown/release/$name.wasm"
